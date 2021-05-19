c0320807d788e0b4987c4d8a2fe1f41d570623f5
Sze Ying <ting.szeying@gmail.com>
2021-05-19 14:07:45 +0800

    feat: add youtube link

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -20,16 +20,16 @@ export const Profile = () => {
           Hello! I am a <span class={`profile-content__accent--${colorTheme}`}>Software Engineer</span> from
           Singapore. I don't want to restrict myself to one field just yet, but I am currently dabbling most
           in web development. I graduated from National University of Singapore in 2019 with a Double Degree
-          in Computer Science and Business Administration. I volunteer regularly at BLESS to help improve literacy rates
-          of young low-income children.
+          in Computer Science and Business Administration. I volunteer regularly at BLESS to help improve
+          literacy rates of young low-income children.
         </p>
         <div className="profile-content__links">
           <ProfileLink url="https://github.com/szenius" text="projects" />
           <ProfileLink url="https://dev.to/szenius" text="dev blog" />
           <ProfileLink url="https://twitter.com/szenius" text="twitter" />
           <ProfileLink url="https://linkedin.com/in/sze-ying-ting" text="linkedin" />
-          <ProfileLink url="https://medium.com/@szenius" text="ded medium" />
-          <ProfileLink url="mailto:ting.szeying@gmail.com" text="talk to me" />
+          <ProfileLink url="https://www.youtube.com/channel/UCiQMR1Ro4iRQpjuN_tzRD3w" text="youtube" />
+          <ProfileLink url="mailto:ting.szeying@gmail.com" text="email" />
         </div>
       </div>
     </div>
