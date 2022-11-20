3492b4d8d71b3b74c7738d16844021214ff56815
Sze Ying <ting.szeying@gmail.com>
2022-11-20 22:34:42 +0800

    feat: update profile

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,10 +1,10 @@
-import React, {useContext} from 'react';
-import {ProfileLink} from './ProfileLink';
-import './profile.css';
-import {Context} from '../../context/ContextProvider';
+import React, { useContext } from "react";
+import { ProfileLink } from "./ProfileLink";
+import "./profile.css";
+import { Context } from "../../context/ContextProvider";
 
 export const Profile = () => {
-  const {state} = useContext(Context);
+  const { state } = useContext(Context);
   const colorTheme = state.colorTheme;
 
   return (
@@ -17,17 +17,24 @@ export const Profile = () => {
           🌻🌻🌻
         </span>
         <p class="profile-content__paragraph">
-          Hello! I am a <span class={`profile-content__accent--${colorTheme}`}>Software Engineer</span> from
-          GovTech Singapore. I am currently dabbling in web and mobile development. I graduated from National University of Singapore in 2019 with a Double Degree
-          in Computer Science and Business Administration. I volunteer regularly at BLESS to help improve
-          literacy rates of young low-income children.
+          Hello! I am a{" "}
+          <span class={`profile-content__accent--${colorTheme}`}>
+            Software Engineer
+          </span>{" "}
+          from Apple. I was previously from GovTech Singapore. I graduated from
+          National University of Singapore in 2019 with a Double Degree in
+          Computer Science and Business Administration. I volunteer regularly at
+          BLESS to help improve literacy rates of young low-income children.
         </p>
         <div className="profile-content__links">
           <ProfileLink url="https://github.com/szenius" text="projects" />
           <ProfileLink url="https://dev.to/szenius" text="dev blog" />
           <ProfileLink url="https://twitter.com/szenius" text="twitter" />
-          <ProfileLink url="https://linkedin.com/in/sze-ying-ting" text="linkedin" />
-          <ProfileLink url="https://www.youtube.com/user/jellyboowow?sub_confirmation=1" text="youtube" />
+          <ProfileLink
+            url="https://linkedin.com/in/sze-ying-ting"
+            text="linkedin"
+          />
+          <ProfileLink url="https://www.youtube.com/@szenius" text="youtube" />
           <ProfileLink url="mailto:ting.szeying@gmail.com" text="email" />
         </div>
       </div>
